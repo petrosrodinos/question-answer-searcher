58a5ceab7829c04c2bdc59f667b38e4d74b297e5
Petros <petros.rodinos@yahoo.com>
2023-08-07 16:41:05 +0300

    added speech to text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import { questions } from "./questions";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState<string>("");
+
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
 
   const handleClick = () => {
     const questionIndex = questions.findIndex((question) =>
@@ -22,6 +30,11 @@ function App() {
     <div className="container">
       <input type="text" onChange={handleChange} value={input} />
       <button onClick={handleClick}>find</button>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={() => SpeechRecognition.startListening()}>Start</button>
+      <button onClick={() => SpeechRecognition.stopListening()}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
       {questions.map((question, index) => (
         <div id={index.toString()} key={index}>
           <h1>{question.question}</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [babel()],
+});
